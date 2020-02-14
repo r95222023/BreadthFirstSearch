cd9cdb644c83c119a0f1752a5bf46628a6512c5b
bo-yanhuang <r95222023@gmail.com>
2020-02-14 17:54:33 -0600

    add test result to test file

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ It provides:
 - initial state parameter for parallel computation
 - solvable check
 
+
+Usage:
+    const NPuzzle = require('./nPuzzle');
+    initState = {'8-6-7-2-5-4-3-0-1': null};
+    eightpuzzle = new NPuzzle(initState);
+    eightpuzzle.engage();
+
+for 8 puzzle:
+'1-2-3-4-5-6-7-8-0' represent
+  123
+  456
+  780
+and similar for 15 puzzle.
+
 Tests can then be run after installation of NodeJs with:
 
     node test
--- test.js
+++ test.js
@@ -2,8 +2,8 @@ const NPuzzle = require('./nPuzzle');
 //8-puzzle test
 initState = {'8-6-7-2-5-4-3-0-1': null};
 // initState = {'1-2-3-4-0-5-7-8-6': null};
+
 eightpuzzle = new NPuzzle(initState);
-state= [8,0,1,3,4,7,2,6,5];
 eightpuzzle.engage();
 // start with: 8,6,7,2,5,4,3,0,1
 // Bingo!!
@@ -16,7 +16,6 @@ eightpuzzle.engage();
 initState = {'1-2-3-4-5-0-7-8-9-6-10-12-13-14-11-15': null};
 fifteenpuzzle = new NPuzzle(initState);
 fifteenpuzzle.engage();
-
 // start with: 1,2,3,4,5,0,7,8,9,6,10,12,13,14,11,15
 // Bingo!!
 // Total time: 2ms
