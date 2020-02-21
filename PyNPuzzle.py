c0108ea22ea1bc15be9d1c1a557615438be0e3ce
bo-yanhuang <r95222023@gmail.com>
2020-02-20 18:04:33 -0600

    make python version a class

--- PyNPuzzle.py
+++ PyNPuzzle.py
@@ -1,26 +1,25 @@
-import numpy as np
+import sys
 import math as math
+import time
+
 history = {}
-
-
-def get_goal_state(n):
-    goalstate = []
-    for i in range(0, n**2-1):
-        goalstate.append(i+1)
-    goalstate.append(0)
-    return goalstate
+# history record every explored state with key equal to state id and value equal to the previous state id of that state.
+# It is used for both hashing and rebuilding entire solution.
 
 
 def get_state_id(state):
+    """Create a unique id for hashing"""
     return str(state)
 
 
 def move(state, pos, rel_pos):
+    """Move zero to a new position with rel_pos relative to current position"""
     new_state = state.copy()
     return swap(new_state, pos, pos + rel_pos)
 
 
 def swap(state, from_pos, to_pos):
+    """Swap zero from current position to a new position"""
     cache = state[from_pos]
     state[from_pos] = state[to_pos]
     state[to_pos] = cache
@@ -38,6 +37,7 @@ def swap(state, from_pos, to_pos):
 #     return res
 
 def compare(state):
+    """Compare current state to the final solution"""
     res = True
     for i in range(0, len(state)-1):
         if state[i] != i+1:
@@ -69,6 +69,8 @@ def is_solvable(state):
 
 
 def get_moves(side_size, col, row):
+    """Create dict for moves that has the has the information such as
+       relative position for zero to swap and is that move possible for each move"""
     return {
         'up': {
             'rel_pos': -1*side_size,
@@ -90,6 +92,7 @@ def get_moves(side_size, col, row):
 
 
 def get_children(state):
+    """Create all possible unexplored node from current state"""
     children = []
     side_size = int(math.sqrt(len(state)))
     pos = state.index(0)
@@ -109,6 +112,7 @@ def get_children(state):
 
 
 def trace_back(state):
+    """Find previous state recursively to rebuild the solution"""
     state_id = get_state_id(state)
     steps = [state_id]
     if state_id in history:
@@ -127,6 +131,7 @@ def trace_back(state):
 
 
 def read_move(steps):
+    """Convert steps to human readable moves"""
     res = []
     size = len(steps[0])
     side_size = int(math.sqrt(size))
@@ -136,18 +141,19 @@ def read_move(steps):
         next_pos = next_state.index(0)
         pos = state.index(0)
         rel = next_pos-pos
-        direction = 'up'
+        direction = 'down'
         if rel == 1:
             direction = 'right'
         if rel == -1:
             direction = 'left'
         if rel == side_size:
-            direction = 'down'
+            direction = 'up'
         res.append(direction)
     return res
 
 
 def breadth_search(states):
+    """Breadth depth first search"""
     cost = 0
     res = []
     children = []
@@ -155,34 +161,47 @@ def breadth_search(states):
         cost = cost + 1
         if compare(state):
             print('Bingo!!')
+            # found solution!
             steps = trace_back(state)
-            res = {'moves:': read_move(steps), 'steps': trace_back(state), 'cost': cost}
+            # rebuild the solution
+            print('Memory Usage: {} bytes'.format(sys.getsizeof(children))) #https://www.pluralsight.com/blog/tutorials/how-to-profile-memory-usage-in-python
+            # check memory usage for the nodes
+            res = {'Depth': len(steps)-1, 'Number of Nodes expanded': cost, 'Moves:': read_move(steps), 'Steps': trace_back(state)}
+            # final result
+            break
+            # stop searching once find the solution
         else:
             for child in get_children(state):
                 children.append(child)
-    return res if ('steps' in res) else children
+
+    return res if ('Steps' in res) else children
 
 
 def engage(state):
+    """engage the computation with 'state' as the initial state"""
+    start_time = time.time()
     history[get_state_id(state)] = False
     search = breadth_search([state])
-    i = 0
     if is_solvable(state) or True:
-        while not ('steps' in search):
-            i = i+1
+        while not ('Steps' in search):
             search = breadth_search(search)
-        print('Depth: {}'.format(i))
-        print('Cost: {}'.format(search['cost']))
-        # print('Moves: {}'.format(search['moves']))
-        print('Steps: {}'.format(search['steps']))
-        print(search['moves'])
+        print(search)
+        # print('Depth: {}'.format(len(search['moves'])))
+        # print('Number of Nodes expanded: {}'.format(search['cost']))
+        # print('Moves: {}'.format(search['Moves']))
+        # print('Steps: {}'.format(search['steps']))
     else:
         print('Warning: the given state is not solvable')
+    print("Time taken: %s seconds " % (time.time() - start_time))
     return
 
 
 class NPuzzle:
+    """Class for NPuzzle"""
     def __init__(self, init_history):
+        # It can take different history for parallel computation.
+        # Ex: {'[1,0,2,3,4,5,6,7,8]':'[0,1,2,3,4,5,6,7,8]','[3,1,2,0,4,5,6,7,8]':'[0,1,2,3,4,5,6,7,8]'}
+        # for two cores machine
         self.history = init_history or {}
         self.is_solvable = is_solvable
         self.get_children = get_children
